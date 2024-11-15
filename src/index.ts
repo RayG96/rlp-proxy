f1ef66f6677a48dada16c9f6c2a85c32da86901a
RayG96 <52840114+RayG96@users.noreply.github.com>
2024-11-15 00:14:34 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -106,7 +106,7 @@ app.get('/v2', async (req, res) => {
       let image = og.image
         ? og.image
         : images.length > 0
-        ? images[0].src
+        ? images[0].url
         : null;
       const description = og.description
         ? og.description
